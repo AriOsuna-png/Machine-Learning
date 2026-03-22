a194729cc003bfe2ff6551db5be49062bf25c42a
Ari Osuna Cota <osunaari7@gmail.com>
2026-03-22 16:33:29 -0700

    Se adecua la API para conectar al frontend y valores relacionados a la predicción

--- BackEnd/api.py
+++ BackEnd/api.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import joblib
 import numpy as np
 import os
@@ -27,13 +26,7 @@ def home():
 # ==============================
 # ENDPOINT DE PREDICCIÓN
 # ==============================
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 
 @app.post("/predict")
 def predict(data: dict):
